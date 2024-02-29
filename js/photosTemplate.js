409923ae18e90e0b0fefc6eb9387c60eb0ff1689
GeneralGLA-true <artemstarodub@gmail.com>
2024-02-29 17:34:11 +0200

    last-tasc

--- js/photosTemplate.js
+++ js/photosTemplate.js
@@ -5,6 +5,12 @@ const pictureContainer = document.createDocumentFragment();
 const container = document.querySelector('.pictures.container');
 const filterSection = document.querySelector('.img-filters--inactive');
 
+const buttonDefauLT = document.querySelector('#filter-default');
+const buttonTopComents = document.querySelector('#filter-discussed'); 
+const buttonRandom = document.querySelector('#filter-random')
+
+
+
 function getPhoto (obj) {
     const cloneTemplate = template.content.cloneNode(true);
     const image = cloneTemplate.querySelector('.picture__img');
@@ -17,12 +23,59 @@ function getPhoto (obj) {
     return cloneTemplate;
 };
 
-function getAllPhotos () {
-    mockArray.map((e) => {pictureContainer.appendChild(getPhoto(e))});
+function getAllPhotos (array = mockArray) {
+    const pictures = container.querySelectorAll('a');
+    
+    if(pictures){
+        pictures.forEach((e)=>e.remove());
+    };
+   
+    array.map((e) => {pictureContainer.appendChild(getPhoto(e))});
     container.appendChild(pictureContainer);
     filterSection.classList.remove('img-filters--inactive');
+};
+
+getAllPhotos();
+
+
+
+function sortByComents (a, b){
+    if (a.coments.length > b.coments.length) return -1;
+    if (a.coments.length < b.coments.length) return 1;
+    return 0;
 }
 
-getAllPhotos()
+function getTopTenComents (){
+    const sorted = JSON.parse(JSON.stringify(mockArray));
+    sorted.sort(sortByComents)
+    sorted.splice(10)
+    getAllPhotos(sorted);
+    buttonRandom.classList.remove('img-filters__button--active');
+    buttonDefauLT.classList.remove('img-filters__button--active');
+    buttonTopComents.classList.add('img-filters__button--active');
+   
+}
+
+function getRandomPictures (){
+    const randomPictures = new Set;
+    const maxPic = 10;
+    do {
+        randomPictures.add(mockArray[Math.floor(Math.random()*mockArray.length)])
+    } while (randomPictures.size < maxPic);
+    getAllPhotos(Array.from(randomPictures));
+    buttonRandom.classList.add('img-filters__button--active');
+    buttonDefauLT.classList.remove('img-filters__button--active');
+    buttonTopComents.classList.remove('img-filters__button--active');  
+}
+
+function getDefault (){
+    getAllPhotos();
+    buttonRandom.classList.remove('img-filters__button--active');
+    buttonDefauLT.classList.add('img-filters__button--active');
+    buttonTopComents.classList.remove('img-filters__button--active');
+}
 
 
+buttonDefauLT.addEventListener('click',()=>getDefault());
+buttonTopComents.addEventListener('click',()=>getTopTenComents());
+buttonRandom.addEventListener('click',()=> getRandomPictures());
